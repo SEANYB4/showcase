d10c39aa2b5ebf8a67c8f844b6bd2fa0404fd9c2
SEANYB4 <bainsean@outlook.com>
2022-09-16 21:59:06 +0800

    Finished portfolio component.

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -32,7 +32,12 @@ const Portfolio = () => {
                                 ></img>
 
                                 <div className='content'>
-                                    <p>{port.description}</p>
+                                    <p className='title'>{port.title}</p>
+                                    <h4 className="description">{port.description}</h4>
+                                    <button
+                                    className='btn'
+                                    onClick={() => window.open(port.url)}
+                                    >Visit site</button>
                                 </div>
                             </div>
                             
